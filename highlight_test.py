6106aca83be266a7d22ad62768347c74ada39f69
Scott Rothrock <scott.rothrock@kraken.tech>
2025-06-11 09:57:01 +0900

    Add Python constants to check Github highlighting rules

---
+++ highlight_test.py
@@ -0,0 +1,4 @@
+NORMAL_HIGHLIGHTING = "This should have the `constant` highlighting"
+EXPECTED_NORMAL_WITH_NUMB3R = "This should be the same `constant` highlighting even though it contains a number"
+EXPECTED_DIFFERENT_WITH_S3 = "This should duplicate known highlighting change behavior when `S3` is in the name"
+EXPECTED_NORMAL_WITH_SES = "This tests whether it's an AWS service that changes the highlighting"
